6403f7405b9b71217e927085a86ecf5bb6e72468
Kristen Shea <kstar61@live.com>
2019-04-02 18:59:37 -0500

    .

--- ipalindrome/Program.cs
+++ ipalindrome/Program.cs
@@ -2,20 +2,18 @@
 
 namespace ipalindrome
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             //Write a C# program to check if a given string is a palindrome or not. Go to the editor
             //Sample Example:
             //For 'aaa' the output should be true
             //For 'abcd' the output should be false
 
-
             Console.Write("Please type out a string of letters: ");
             string x = Console.ReadLine().ToLower();
             bool answer = Palindrome(x);
-            
 
             if (answer)
             {
@@ -26,19 +24,15 @@ namespace ipalindrome
                 Console.WriteLine("No, this is not a palindrome.");
             }
 
-
             Console.ReadLine();
-
-
         }
+
         private static bool Palindrome(string x)
         {
             char[] y = x.ToCharArray();
             Array.Reverse(y);
 
             return new string(y).Equals(x);
-
-
         }
     }
 }
